f2136b1e6fa2cb43077e5511a631db8650b63080
concept <conceptcodes@gmail.com>
2023-08-12 14:08:23 -0400

    test

---
+++ jest.config.mjs
@@ -0,0 +1,16 @@
+// @ts-nocheck
+import nextJest from "next/jest.js"
+
+const createJestConfig = nextJest({
+  dir: "./",
+})
+
+/** @type {import('jest').Config} */
+const config = {
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  testEnvironment: "jest-environment-jsdom",
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  testPathIgnorePatterns: ["<rootDir>/e2e"],
+}
+
+export default createJestConfig(config)
